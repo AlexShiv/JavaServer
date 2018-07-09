a4a5ab65b1ded3dd1858e49d85243157cc8bb736
AlexShiv <Shiyanov1998@mail.ru>
2018-07-09 18:16:40 +0400

    hello world server

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Frontend frontend = new Frontend();
+
+        Server server = new Server(8080); // основной класс Jetty сервера
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(frontend), "/authform");
+
+        server.start();
+        try {
+            server.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
